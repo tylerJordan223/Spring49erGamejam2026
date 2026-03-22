96f37d260456d0914f3d71b0a891b0190c4f523f
tylerJordan223 <tjlinkmaster22@gmail.com>
2026-03-21 20:19:12 -0400

    Added Doves

---
+++ Spring49erGamejam2026/Assets/Scripts/Enemies/DoveScript.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class DoveScript : MonoBehaviour
+{
+    [SerializeField] GameObject card_drop;
+
+    public float speed;
+    private Transform player;
+
+    private void Start()
+    {
+        player = MagicianController.instance.player_obj.transform;
+    }
+
+    private void FixedUpdate()
+    {
+        MoveDove();
+    }
+
+    private void MoveDove()
+    {
+        Vector3 direction = player.position - transform.position;
+
+        transform.position += direction.normalized * Time.deltaTime * speed;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //if its hit by the weapons
+        if (collision.CompareTag("Weapon"))
+        {
+            //spawn a card and place it at the bunny
+            GameObject c = Instantiate(card_drop);
+            c.transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+
+            //kill the bunny
+            Destroy(this.gameObject);
+        }
+    }
+}
